6be8b219e5eef0bcee74285d1794c38e97dff673
Jensa666 <boggelhauers@gmail.com>
2025-10-05 19:59:30 +0200

    Create _middleware.js

---
+++ functions/_middleware.js
@@ -0,0 +1,10 @@
+export const onRequest = async ({ request, next }) => {
+  const res = await next();
+  const hdr = new Headers(res.headers);
+  hdr.set('X-Frame-Options','DENY');
+  hdr.set('Referrer-Policy','strict-origin-when-cross-origin');
+  hdr.set('X-Content-Type-Options','nosniff');
+  hdr.set('Permissions-Policy','geolocation=()');
+  hdr.set('Content-Security-Policy', "default-src 'self'; img-src 'self' data:; style-src 'self' 'unsafe-inline'; script-src 'self' https://cdn.jsdelivr.net; connect-src 'self'");
+  return new Response(res.body, {status: res.status, headers: hdr});
+};
